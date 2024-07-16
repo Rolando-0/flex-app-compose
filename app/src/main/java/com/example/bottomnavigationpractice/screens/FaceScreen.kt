7e81de8646ed810d9b4beb65acb5d9745cb6c27c
reinc <reincandel@gmail.com>
2024-07-15 23:38:51 -0400

    Added the One rep max func and drop down

--- app/src/main/java/com/example/bottomnavigationpractice/screens/FaceScreen.kt
+++ app/src/main/java/com/example/bottomnavigationpractice/screens/FaceScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,6 +19,9 @@ import com.example.bottomnavigationpractice.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FaceScreen() {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf("Calorie Calculator") }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -24,7 +30,7 @@ fun FaceScreen() {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
-                    Text("  Calorie Calculator")
+                    Text("Calorie Calculator")
                 },
                 navigationIcon = {
                     Icon(
@@ -32,11 +38,116 @@ fun FaceScreen() {
                         contentDescription = null,
                         modifier = Modifier.size(32.dp) // Adjust the size as needed
                     )
+                },
+                actions = {
+                    Box(modifier = Modifier.padding(end = 16.dp)) {
+                        IconButton(onClick = { expanded = true }) {
+                            Icon(Icons.Default.MoreVert, contentDescription = null)
+                        }
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                           DropdownMenuItem(
+                               text = { Text(text = "Calorie Calculator") },
+                               onClick = {selectedItem = "Calorie Calculator"},
+                           )
+                            DropdownMenuItem(
+                                text = { Text(text = "One Rep Max Calculator") },
+                                onClick = {selectedItem = "One Rep Max Calculator"  },
+                            )
+                        }
+                    }
                 }
             )
         },
     ) { innerPadding ->
-        CalcContent(innerPadding)
+        when (selectedItem) {
+            "Calorie Calculator" -> CalcContent(innerPadding)
+            "One Rep Max Calculator" -> OneRepMaxCalculatorContent(innerPadding)
+            else -> {}
+        }
+    }
+}
+
+@Composable
+fun OneRepMaxCalculatorContent(innerPadding: PaddingValues) {
+    var weight by remember { mutableStateOf(TextFieldValue()) }
+    var reps by remember { mutableStateOf(TextFieldValue()) }
+    var result by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .padding(16.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        BasicTextField(
+            value = weight,
+            onValueChange = { weight = it },
+            decorationBox = { innerTextField ->
+                Row(
+                    Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface))
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (weight.text.isEmpty()) {
+                        Text("Weight (lbs)")
+                    }
+                    innerTextField()
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        BasicTextField(
+            value = reps,
+            onValueChange = { reps = it },
+            decorationBox = { innerTextField ->
+                Row(
+                    Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface))
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (reps.text.isEmpty()) {
+                        Text("Repetitions")
+                    }
+                    innerTextField()
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                val weightValue = weight.text.toFloatOrNull()
+                val repsValue = reps.text.toIntOrNull()
+
+                if (weightValue != null && repsValue != null) {
+                    val oneRepMax = weightValue * (1 + repsValue / 30f)
+                    result = "1 X ${oneRepMax.toInt()} is your One Rep Max"
+                } else {
+                    result = "Please enter valid values for weight and repetitions."
+                }
+            }
+        ) {
+            Text("Calculate")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(result)
     }
 }
 
@@ -76,7 +187,6 @@ fun CalcContent(innerPadding: PaddingValues) {
         BasicTextField(
             value = weight,
             onValueChange = { weight = it },
-            singleLine = true,
             decorationBox = { innerTextField ->
                 Row(
                     Modifier
@@ -100,7 +210,6 @@ fun CalcContent(innerPadding: PaddingValues) {
         BasicTextField(
             value = height,
             onValueChange = { height = it },
-            singleLine = true,
             decorationBox = { innerTextField ->
                 Row(
                     Modifier
@@ -123,7 +232,6 @@ fun CalcContent(innerPadding: PaddingValues) {
 
         BasicTextField(
             value = age,
-            singleLine = true,
             onValueChange = { age = it },
             decorationBox = { innerTextField ->
                 Row(
