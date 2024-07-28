746edf9a410dcfbe984065a58b5dde5302045864
Rolando-0 <rolando3ar@aol.com>
2024-07-27 21:51:11 -0400

    final comments

--- app/src/main/java/com/example/flexapp/MainActivity.kt
+++ app/src/main/java/com/example/flexapp/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.flexapp.ui.theme.FlexAppTheme
 
 
