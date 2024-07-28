cf30801ad0c308211576e1c68b32738675cfad0a
Rolando-0 <rolando3ar@aol.com>
2024-07-27 21:50:54 -0400

    final comments

--- app/src/main/java/com/example/flexapp/MainActivity.kt
+++ app/src/main/java/com/example/flexapp/MainActivity.kt
@@ -1,5 +1,13 @@
 /**
- * The main class which launches the app
+ * The main class which launches the app,
+ * its colors, fonts and themes are defined in the ui.theme package,
+ *
+ * the database implementation of the app is defined in the database package
+ *
+ * the screens in the app are defined in the screens package, and their
+ * ViewModels are defined in the viewmodels subpackage
+ *
+ * the navigation between the screens is defined in the navigation package
  * */
 
 package com.example.flexapp
